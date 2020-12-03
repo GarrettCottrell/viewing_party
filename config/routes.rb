ce4a38fa33395c7cf63cb54f6842fe31f07abe90
Nick King <nickmaxking@gmail.com>
2020-12-03 16:22:59 -0700

    Update routes with resources syntax. Change /dashboard and /discover paths to restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # welcome
+  get '/', to: 'welcome#index', as: :welcome
+
+  # users
   get '/registration', to: 'users#new', as: :registration
   resources :users, only: %i[create]
-  get '/dashboard', to: 'dashboard#show'
-  get '/discover', to: 'movies#new'
-  get '/movies', to: 'movies#index'
-  get '/', to: 'welcome#index'
-  post '/sessions', to: 'sessions#create'
-  get '/movies/:id', to: 'movies#show' #as: :movie - will change it into movie path
 
-  #events
-  get '/events/new', to: 'events#new'
+  # dashboard
+  resources :dashboard, only: %i[index]
+
+  # discover
+  resources :discover, only: %i[index]
+
+  # movies
+  resources :movies, only: %i[index show]
+
+  # sessions
+  resources :sessions, only: %i[create]
+
+  # events
+  resources :events, only: %i[new]
 end

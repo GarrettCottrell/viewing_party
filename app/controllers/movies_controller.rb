3b73cc5cef4f7bb996f1cd95626e360dc02fccd7
Nick King <nickmaxking@gmail.com>
2020-12-02 21:40:58 -0700

    Update movies show action with movie_details API consumption
    Co-authored-by: Garrett Cottrell <49660931+GarrettCottrell@users.noreply.github.com>
    Co-authored-by: Shaun James <58994078+ShaunDaneJames@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    conn = Faraday.new(url:'https://api.themoviedb.org/3/movie?api_key=fffb60883e9ce66c7fd35f9e77ec9682' )
+    conn = Faraday.new(url:'https://api.themoviedb.org/3/movie?api_key=fffb60883e9ce66c7fd35f9e77ec9682')
     @movies = []
     page = 1
 
@@ -31,5 +31,10 @@ class MoviesController < ApplicationController
     end
   end
 
-  def show; end
+  def show
+    conn = Faraday.new(url:'https://api.themoviedb.org/3/movie?api_key=fffb60883e9ce66c7fd35f9e77ec9682')
+
+    response = conn.get("/3/movie/#{params[:id]}")
+    @movie_details = JSON.parse(response.body, symbolize_names: true)
+  end
 end

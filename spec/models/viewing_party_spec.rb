9eba85c2aaa62bb8a4a87c3c0ee6faccfb96ade4
Shaun James <ShaunDaneJames@Gmail.com>
2020-12-05 12:25:14 -0700

    Adds test for ViewingParty and includes validations for date, start time, duration, and host id.
    Co-authored-by: Nick King <68172332+nmking22@users.noreply.github.com>
    Co-authored-by: Garret Cottrell <49660931+GarrettCottrell@users.noreply.github.com>

---
+++ spec/models/viewing_party_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe ViewingParty, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :date}
+    it { should validate_presence_of :start_time}
+    it { should validate_presence_of :duration}
+    it { should validate_presence_of :host_id}
+  end
+end

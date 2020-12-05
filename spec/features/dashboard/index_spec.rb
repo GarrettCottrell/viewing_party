73234881e3d1f3d6df00b302e61792fc6389ae67
Shaun James <ShaunDaneJames@Gmail.com>
2020-12-05 16:10:59 -0700

    Adds test for new viewing party form to prepopulate with movie title and movie duration.
    Co-authored-by: Nick King <68172332+nmking22@users.noreply.github.com>
    Co-authored-by: Garret Cottrell <49660931+GarrettCottrell@users.noreply.github.com>

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -39,6 +39,24 @@ require 'rails_helper'
       expect(page).to have_content(viewing_party_one.start_time)
       expect(page).to have_content('Invited')
     end
+
+    it 'I see movie title, date and time and status of hosting for all of
+    the viewing parties that I am hosting' do
+      shawshank = Movie.create(name: 'Shawshank Redemption', api_id: 1337)
+      viewing_party_one = Event.create!(movie_id: shawshank.id,
+                                        date: '12/5/2020',
+                                        start_time: '12:00',
+                                        duration: "90")
+
+      UserEvent.create!(user_id: @garrett.id,
+                        event_id: viewing_party_one.id,
+                        status: 'Hosting')
+      visit dashboard_index_path
+      expect(page).to have_content(viewing_party_one.movie.name)
+      expect(page).to have_content(viewing_party_one.date)
+      expect(page).to have_content(viewing_party_one.start_time)
+      expect(page).to have_content('Hosting')
+    end
   end
 
   describe 'As a guest' do
---
+++ spec/features/viewing_party/new_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe 'As an authenticated user' do
+  describe 'When I visit the new viewing party page' do
+    before :each do
+      @garrett = User.create!(name: 'Garrett', email: 'garrett.cottrell', password: '1234', password_confirmation: '1234')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@garrett)
+    end
+    it 'should have a form with the following:
+    Movie Title (that is un-editable)
+    Duration of Party with a default value of movie runtime in minutes
+    When: field to select date
+    Start Time: field to select time
+    Checkboxes next to each friend (if user has friends)
+    Button to create a party' do
+
+      visit '/movies/278'
+      click_button 'Create Viewing Party for Movie'
+      expect(current_path).to eq(new_event_path)
+      expect(page).to have_content('Shawshank Redemption')
+      expect(page).to have_content(1337)
+
+      expect(page).to_not have_field('Movie Title')
+      fill_in 'Duration', with: 160
+      fill_in 'Day', with: '12/20/20'
+      fill_in 'Start Time', with: '13:00'
+      #Friends?
+      click_button 'Create Party'
+    end
+  end
+end
+
+# Details When the party is created,
+# the authenticated user should be redirected back to the dashboard where
+# the new event is shown. The event should also be seen by any friends that were
+# invited when they log in.

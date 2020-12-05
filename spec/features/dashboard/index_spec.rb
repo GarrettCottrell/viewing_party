856b433b4b4df6dcc219b1e665df1e39d9e5299a
Shaun James <ShaunDaneJames@Gmail.com>
2020-12-05 12:01:04 -0700

    Adds feature test for Viewing Party.
    Co-authored-by: Nick King <68172332+nmking22@users.noreply.github.com>
    Co-authored-by: Garret Cottrell <49660931+GarrettCottrell@users.noreply.github.com>

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -1,13 +1,8 @@
 require 'rails_helper'
 
-describe 'As an authenticated user' do
   describe "When I visit '/dashboard'" do
-    it 'If I am not not an authenticated user, I see a link to register' do
-      visit '/dashboard'
-      expect(page).to have_link('Register')
-    end
-
-    before :each do 
+    describe 'As an authenticated user' do
+    before :each do
       garrett = User.create(name: 'Garrett', email: 'garrett.cottrell', password: '1234')
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(garrett)
     end
@@ -29,12 +24,19 @@ describe 'As an authenticated user' do
 
     it 'I see movie title, date and time and status of invited for all of
     the viewing parties that I am invited to' do
-      shawshank = Movie.create(movie_title: 'Shawshank Redemption' )
+      shawshank = Movie.create(movie_title: 'Shawshank Redemption', api_id: 1337)
       viewing_party_one = ViewingParty.create!(movie_id: 'Shawshank Redemption', date_and_time: 12/5/2020,  )
       visit dashboard_index_path
       expect(page).to have_content(viewing_party_one.movie_title)
       expect(page).to have_content(viewing_party_one.date_and_time)
       expect(page).to have_content('Invited')
+    end
+  end
+
+  describe 'As a guest' do
+    it 'I see a link to register' do
+      visit '/dashboard'
+      expect(page).to have_link('Register')
     end
   end
 end

40330687859217e13d50d1ec65995ab28f8dfa93
Shaun James <ShaunDaneJames@Gmail.com>
2020-12-05 15:26:34 -0700

    Adds test to determine event status for user dashboards. Also adds information to dashboard index page.
    Co-authored-by: Nick King <68172332+nmking22@users.noreply.github.com>
    Co-authored-by: Garret Cottrell <49660931+GarrettCottrell@users.noreply.github.com>

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
   describe "When I visit '/dashboard'" do
     describe 'As an authenticated user' do
     before :each do
-      garrett = User.create(name: 'Garrett', email: 'garrett.cottrell', password: '1234')
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(garrett)
+      @garrett = User.create!(name: 'Garrett', email: 'garrett.cottrell', password: '1234', password_confirmation: '1234')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@garrett)
     end
 
     it 'I should see a welcome message' do
@@ -29,9 +29,14 @@ require 'rails_helper'
                                         date: '12/5/2020',
                                         start_time: '12:00',
                                         duration: "90")
+
+      UserEvent.create!(user_id: @garrett.id,
+                        event_id: viewing_party_one.id,
+                        status: 'Invited')
       visit dashboard_index_path
       expect(page).to have_content(viewing_party_one.movie.name)
-      expect(page).to have_content(viewing_party_one.date_and_time)
+      expect(page).to have_content(viewing_party_one.date)
+      expect(page).to have_content(viewing_party_one.start_time)
       expect(page).to have_content('Invited')
     end
   end

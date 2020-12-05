40e97b6cbdf394e297d28920137608a67563e148
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-12-05 11:17:59 -0700

    Started test for viewing party dashboard including setting up scenario block for viewing parties I'm invited to.
    Co-authored-by: Nick King <68172332+nmking22@users.noreply.github.com>
    Co-authored-by: Shaun James <58994078+ShaunDaneJames@users.noreply.github.com>

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -2,49 +2,39 @@ require 'rails_helper'
 
 describe 'As an authenticated user' do
   describe "When I visit '/dashboard'" do
-    it 'I should see a welcome message' do
+    it 'If I am not not an authenticated user, I see a link to register' do
+      visit '/dashboard'
+      expect(page).to have_link('Register')
+    end
+
+    before :each do 
       garrett = User.create(name: 'Garrett', email: 'garrett.cottrell', password: '1234')
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(garrett)
+    end
 
+    it 'I should see a welcome message' do
       visit '/dashboard'
 
       expect(page).to have_content('Welcome Garrett!')
     end
 
-    it 'If I am not not an authenticated user, I see a link to register' do
-      visit '/dashboard'
-
-      expect(page).to have_link('Register')
-    end
-
     it "The 'discover movies' button redirects me to '/discover'" do
-      garrett = User.create(name: 'Garrett', email: 'garrett.cottrell', password: '1234')
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(garrett)
-
       visit '/dashboard'
-
       click_button 'Discover Movies'
-
       expect(current_path).to eq('/discover')
     end
 
-    xit "Friends Section" do
-
+    xit 'Friends Section' do
     end
 
-    xit "Viewing Parties" do
-
+    it 'I see movie title, date and time and status of invited for all of
+    the viewing parties that I am invited to' do
+      shawshank = Movie.create(movie_title: 'Shawshank Redemption' )
+      viewing_party_one = ViewingParty.create!(movie_id: 'Shawshank Redemption', date_and_time: 12/5/2020,  )
+      visit dashboard_index_path
+      expect(page).to have_content(viewing_party_one.movie_title)
+      expect(page).to have_content(viewing_party_one.date_and_time)
+      expect(page).to have_content('Invited')
     end
   end
 end
-
-
-
-# As an authenticated user,
-# When I visit '/dashboard'
-# I should see:
-#
-#  'Welcome <username>!' at the top of page
- #9 A button to Discover Movies
- #10 A friends section
- #11 A viewing parties section

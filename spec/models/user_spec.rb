bea5854992afb291c8f704e811354db98eea4e0b
Nick King <nickmaxking@gmail.com>
2020-12-08 12:54:19 -0700

    Add test for find_friend class method
    Co-authored-by: Garrett Cottrell <49660931+GarrettCottrell@users.noreply.github.com>
    Co-authored-by: Shaun James <58994078+ShaunDaneJames@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,4 +16,13 @@ describe User, type: :model do
     it { should have_many :friendships}
     it { should have_many(:friends).through :friendships}
   end
+
+  describe 'class methods' do
+    it '.find_friend' do
+      garrett = User.create!(name: 'Garrett', email: 'garrett.cottrell', password: '1234', password_confirmation: '1234')
+      nick = User.create!(name: 'Nick', email: 'Nick@example.com', password: '1234', password_confirmation: '1234')
+
+      expect(User.find_friend(nick.email)).to eq(nick)
+    end
+  end
 end

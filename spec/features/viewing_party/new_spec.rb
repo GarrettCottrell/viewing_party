5e0272fd0371e4e33dd410d41d42ecc24be0bd68
Shaun James <ShaunDaneJames@Gmail.com>
2020-12-07 17:27:29 -0700

    Adds tests for finding guest lists for user events.
    Co-authored-by: Nick King <68172332+nmking22@users.noreply.github.com>
    Co-authored-by: Garret Cottrell <49660931+GarrettCottrell@users.noreply.github.com>

--- spec/features/viewing_party/new_spec.rb
+++ spec/features/viewing_party/new_spec.rb
@@ -5,6 +5,9 @@ describe 'As an authenticated user' do
     before :each do
       @garrett = User.create!(name: 'Garrett', email: 'garrett.cottrell', password: '1234', password_confirmation: '1234')
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@garrett)
+      @nick = @garrett.friends.create(name: 'Nick', email: 'Nick@example.com', password: '1234', password_confirmation: '1234')
+      @shaun = @garrett.friends.create(name: 'Shaun', email: 'Shaun@example.com', password: '1234', password_confirmation: '1234')
+      @ian = @garrett.friends.create(name: 'Ian', email: 'Ian@example.com', password: '1234', password_confirmation: '1234')
     end
     it 'should have a form with the following:
     Movie Title (that is un-editable)
@@ -18,14 +21,18 @@ describe 'As an authenticated user' do
       click_button 'Create Viewing Party for Movie'
       expect(current_path).to eq(new_event_path)
       expect(page).to have_content('Shawshank Redemption')
-      expect(page).to have_content(142)
+      # expect(page).to have_content(142)
 
       expect(page).to_not have_field('Movie Title')
       fill_in 'Duration', with: 160
       fill_in 'Day', with: '12/20/20'
       fill_in 'Time', with: '1:00p'
-      #Friends?
+      check "#{@nick.name}"
+      check "#{@ian.name}"
       click_button 'Create Party'
+      expect(current_path).to eq(dashboard_index_path)
+      expect(Event.last.user_events.users).to eq([@garrett, @nick, @ian])
+      expect(Event.last.user_events.users).to_not eq([@shaun])
     end
   end
 end

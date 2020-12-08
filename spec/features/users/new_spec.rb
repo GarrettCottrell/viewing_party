9d3eced0ec290a90d38259623ce1a256c0d481dc
Nick King <nickmaxking@gmail.com>
2020-12-08 12:56:53 -0700

    Add sad path testing for partially filled forms
    Co-authored-by: Garrett Cottrell <49660931+GarrettCottrell@users.noreply.github.com>
    Co-authored-by: Shaun James <58994078+ShaunDaneJames@users.noreply.github.com>

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -11,8 +11,20 @@ describe 'As a visitor' do
       fill_in :password_confirmation, with: 'testpassword'
 
       click_button 'Register'
-      expect(current_path).to eq('/dashboard')
+      expect(current_path).to eq(dashboard_index_path)
       expect(page).to have_content('Testname')
+    end
+
+    it 'If I do not completely fill in the form, I am redirected back to users new with a flash message' do
+      visit '/registration'
+
+      fill_in :name, with: 'Testname'
+      fill_in :password, with: 'testpassword'
+      fill_in :password_confirmation, with: 'testpassword'
+
+      click_button 'Register'
+      expect(current_path).to eq(users_path)
+      expect(page).to have_content("Email can't be blank")
     end
 
     it 'I see a link to log-in and when I click that link I am redirected to

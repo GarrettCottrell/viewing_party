2f8552bb0446f70ae296c8ef420d047aff6f1aee
Nick King <nickmaxking@gmail.com>
2020-12-08 16:42:30 -0700

    Add logic to parse through multiple pages
    Co-authored-by: Garrett Cottrell <49660931+GarrettCottrell@users.noreply.github.com>
    Co-authored-by: Shaun James <58994078+ShaunDaneJames@users.noreply.github.com>

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,10 +1,16 @@
 class MovieFacade
   def self.search(title)
-    movies_data = MovieService.find_by_title(title)
-    movies_data.map do |search_data|
-      runtime = MovieService.find_runtime(search_data[:id])
-      MoviePoro.new(search_data, runtime)
+    movies = []
+    page = 1
+    until movies.count >= 40 || page == 3
+      movies_data = MovieService.find_by_title(title, page)
+      movies_data.each do |search_data|
+        runtime = MovieService.find_runtime(search_data[:id])
+        movies << MoviePoro.new(search_data, runtime)
+      end
+      page += 1
     end
+    movies.uniq
   end
 
   def self.top_40

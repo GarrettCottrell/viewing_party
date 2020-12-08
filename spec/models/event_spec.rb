5acd61a56b1905f9e0f4c480c14803f1abce3e72
Nick King <nickmaxking@gmail.com>
2020-12-08 12:55:12 -0700

    Add movie_id validation in model and test
    Co-authored-by: Garrett Cottrell <49660931+GarrettCottrell@users.noreply.github.com>
    Co-authored-by: Shaun James <58994078+ShaunDaneJames@users.noreply.github.com>

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -5,7 +5,7 @@ describe Event, type: :model do
     it { should validate_presence_of :date}
     it { should validate_presence_of :start_time}
     it { should validate_presence_of :duration}
-    #it { should validate_presence_of :host_id}
+    it { should validate_presence_of :movie_id}
   end
 
   describe 'relationships' do

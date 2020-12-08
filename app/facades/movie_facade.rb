e6db1300ebd805c808503436916b3c9a58e61d1f
Nick King <nickmaxking@gmail.com>
2020-12-08 16:55:17 -0700

    Add top_40 class method to MovieFacade
    Co-authored-by: Garrett Cottrell <49660931+GarrettCottrell@users.noreply.github.com>
    Co-authored-by: Shaun James <58994078+ShaunDaneJames@users.noreply.github.com>

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -14,6 +14,16 @@ class MovieFacade
   end
 
   def self.top_40
-
+    movies = []
+    page = 1
+    until movies.count >= 40 || page == 3
+      movies_data = MovieService.top_rated(page)
+      movies_data.each do |search_data|
+        runtime = MovieService.find_runtime(search_data[:id])
+        movies << MoviePoro.new(search_data, runtime)
+      end
+      page += 1
+    end
+    movies.uniq
   end
 end

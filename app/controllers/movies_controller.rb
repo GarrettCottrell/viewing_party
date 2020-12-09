df4c91bd4857b80e753ec48e79daccc440797657
Nick King <nickmaxking@gmail.com>
2020-12-08 20:09:05 -0700

    Refactor movie show action with new service, facade, and poro functionality. Remove unnecessary figaro setup
    Co-authored-by: Garrett Cottrell <49660931+GarrettCottrell@users.noreply.github.com>
    Co-authored-by: Shaun James <58994078+ShaunDaneJames@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,3 @@
-# require 'figaro'
-# Figaro.application = Figaro::Application.new(environment: 'production', path: File.expand_path('config/application.yml', __dir__))
-# Figaro.load
 class MoviesController < ApplicationController
   def index
     if params[:Search_By_Movie_Title]
@@ -12,24 +9,6 @@ class MoviesController < ApplicationController
   end
 
   def show
-    conn = Faraday.new(
-      url: 'https://api.themoviedb.org/3/movie',
-      params: { api_key: ENV['viewing_party_key'] }
-    )
-
-    response = conn.get("/3/movie/#{params[:id]}")
-    @movie_details = JSON.parse(response.body, symbolize_names: true)
-    @genres = @movie_details[:genres].map do |genre|
-      genre[:name]
-    end.join(', ')
-
-    response = conn.get("/3/movie/#{params[:id]}/credits")
-    json = JSON.parse(response.body, symbolize_names: true)
-    @credits = json[:cast].first(10)
-
-    response = conn.get("/3/movie/#{params[:id]}/reviews")
-    @reviews = JSON.parse(response.body, symbolize_names: true)
-    # can also make helper methods here for setting up api calls
-    # Maybe? response = conn.get(cast_members)
+    @movie = MovieFacade.get_details(params[:id])
   end
 end

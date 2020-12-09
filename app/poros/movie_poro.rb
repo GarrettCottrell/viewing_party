9fb9213b1e923453dd820bd769e2474f0de2fec1
Nick King <nickmaxking@gmail.com>
2020-12-08 20:02:22 -0700

    Update MoviePoro to work with all relevant API endpoints. Add methods to view all movie info
    Co-authored-by: Garrett Cottrell <49660931+GarrettCottrell@users.noreply.github.com>
    Co-authored-by: Shaun James <58994078+ShaunDaneJames@users.noreply.github.com>

--- app/poros/movie_poro.rb
+++ app/poros/movie_poro.rb
@@ -1,10 +1,71 @@
 class MoviePoro
-  attr_reader :title, :id, :vote_average, :runtime
-  
-  def initialize(search_data, runtime)
-    @title = search_data[:title]
-    @id = search_data[:id]
-    @vote_average = search_data[:vote_average]
-    @runtime = runtime
+  attr_reader :search_data,
+              :details_data,
+              :cast,
+              :reviews_data
+
+  def initialize(search_data = nil, details_data = nil, cast = nil, reviews_data = nil)
+    @search_data = search_data
+    @details_data = details_data
+    @cast = cast
+    @reviews_data = reviews_data
+  end
+
+  def title
+    @search_data[:title]
+  end
+
+  def id
+    @search_data[:id]
+  end
+
+  def vote_average
+    @search_data[:vote_average]
+  end
+
+  def genres
+    if @details_data[:genres].empty?
+      'No genres found.'
+    else
+      @details_data[:genres].map do |genre|
+        genre[:name]
+      end.join(', ')
+    end
+  end
+
+  def runtime
+    @details_data[:runtime]
+  end
+
+  def runtime_display
+    if @details_data[:runtime] == 0
+      'Runtime information unavailable.'
+    else
+      "#{(@details_data[:runtime] / 60)} hr #{(@details_data[:runtime] % 60)} min"
+    end
+  end
+
+  def total_reviews
+    @reviews_data[:total_results]
+  end
+
+  def reviews
+    @reviews_data[:results]
+  end
+
+  def api_id
+    @details_data[:id]
+  end
+
+  def original_title
+    @details_data[:original_title]
+  end
+
+  def average_vote
+    @details_data[:vote_average]
+  end
+
+  def overview
+    @details_data[:overview]
   end
 end

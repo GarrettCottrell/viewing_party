e7f32ce7fa219aeaf3712b507aa2d83926526cf6
Nick King <nickmaxking@gmail.com>
2020-12-09 22:04:49 -0700

    Extract make_valid_friendship helper method

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,16 +2,21 @@ class FriendshipsController < ApplicationController
   def create
     if User.user_exists?(params[:friend_search])
       friend = User.find_friend(params[:friend_search])
-      friendship = Friendship.new(user: current_user, friend: friend)
-      if current_user.valid_friendship?(friend)
-        friendship.save
-        flash[:success] = "#{friend.name} successfully added as a friend"
-      else
-        flash[:error] = "New friends can't be yourself or existing friends"
-      end
+      make_valid_friendship(friend)
     else
       flash[:error] = 'User does not exist in the database'
     end
     redirect_to dashboard_index_path
+  end
+
+  private
+
+  def make_valid_friendship(friend)
+    if current_user.valid_friendship?(friend)
+      Friendship.create(user: current_user, friend: friend)
+      flash[:success] = "#{friend.name} successfully added as a friend"
+    else
+      flash[:error] = "New friends can't be yourself or existing friends"
+    end
   end
 end

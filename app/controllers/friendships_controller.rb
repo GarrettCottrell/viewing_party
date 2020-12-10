1664bc08d54376135913543b3c792259875e7ec2
Nick King <nickmaxking@gmail.com>
2020-12-09 21:24:23 -0700

    Add logic for friendship creation edge cases

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,9 +1,14 @@
 class FriendshipsController < ApplicationController
   def create
-    friend = User.find_friend(params[:friend_search])
-    friendship = Friendship.new(user: current_user, friend: friend)
-    if friendship.save
-      flash[:success] = "#{friend.name} successfully added as a friend"
+    if User.user_exists?(params[:friend_search])
+      friend = User.find_friend(params[:friend_search])
+      friendship = Friendship.new(user: current_user, friend: friend)
+      if current_user.valid_friendship?(friend)
+        friendship.save
+        flash[:success] = "#{friend.name} successfully added as a friend"
+      else
+        flash[:error] = "New friends can't be yourself or existing friends"
+      end
     else
       flash[:error] = 'User does not exist in the database'
     end

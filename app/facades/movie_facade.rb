1a3cd988652a837e1c516af6e1d73094e6106e3f
Nick King <nickmaxking@gmail.com>
2020-12-09 22:46:07 -0700

    Change method names to make rubocop happy

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,13 +1,12 @@
 class MovieFacade
   def self.search(title)
     movies = []
-    page = 1
     until movies.count >= 40
-      movies_data = MovieService.find_by_title(title, page)
+      movies_data = MovieService.search_by_title(title, page ||= 1)
       movies_data.each do |search_data|
         movies << MoviePoro.new(search_data)
       end
-      # leave comment here explaining the usage of the number 20
+      # the magic number 20 is used here, because that is the default yield per page from this API
       break if movies_data.length < 20
 
       page += 1
@@ -16,11 +15,10 @@ class MovieFacade
   end
 
   # change method name to top_movies(# of movies)
-  def self.top_40
+  def self.top_forty
     movies = []
-    page = 1
     until movies.count >= 40
-      movies_data = MovieService.top_rated(page)
+      movies_data = MovieService.top_rated(page ||= 1)
       movies_data.each do |search_data|
         movies << MoviePoro.new(search_data)
       end
